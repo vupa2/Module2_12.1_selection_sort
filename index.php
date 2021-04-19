c2c265b72ce82202b045dc7d709a129d6f65cd1e
vupa2 <anhvu.pa96@gmail.com>
2021-04-19 11:03:07 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$data = [1, 9, 4.5, 6.6, 5.7, -4.5];
+
+function selectionSort($data) {
+    $dataLength = count($data);
+    for ($i = 0; $i < $dataLength - 1; $i++) {
+        $min = $i;
+        for ($j = $i + 1; $j < $dataLength; $j++) {
+            if ($data[$j] < $data[$min]) $min = $j;
+        }
+        $temp = $data[$min];
+        $data[$min] = $data[$i];
+        $data[$i] = $temp;
+    }
+    return $data;
+}
+
+var_dump(selectionSort($data));
